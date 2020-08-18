3ab4086ffcc57623d304eefc5057c50552f933d6
Kuliza-280 <psnitsri01@gmail.com>
2020-08-18 12:12:18 +0530

    Adding profanity checks in comments using bad-words

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 const translate = require("moji-translate");
+const Filter = require("bad-words");
+const profanityFilter = new Filter({ placeHolder: "🤐" });
 
 async function run() {
   const githubToken = core.getInput("GITHUB_TOKEN");
+  const enablePolice = core.getInput("enablePolice");
   const octokit = github.getOctokit(githubToken);
   const { eventName, repo, payload } = github.context;
+
+  console.log(enablePolice);
 
   switch (eventName) {
     case "issue_comment":
       const issueComment = payload.comment.body;
-      const body = translate.translate(issueComment);
+      let filteredComment = issueComment;
+      if (enablePolice === "true")
+        filteredComment = profanityFilter(issueComment);
+      const body = translate.translate(filteredComment);
       octokit.issues
         .updateComment({ ...repo, comment_id: payload.comment.id, body })
         .then(() => core.info("✔ Done."))
